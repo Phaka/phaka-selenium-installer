e8afb14f47c9b25f974a2f06c404d110ed5a4d49
Werner Strydom <werners@bloudraak.com>
2018-01-04 18:44:03 -0800

    Improved Scripts

--- scripts/node/install.ps1
+++ scripts/node/install.ps1
@@ -1,1 +1,5 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install jre8
+choco install firefox
+choco install googlechrome
+choco install opera
